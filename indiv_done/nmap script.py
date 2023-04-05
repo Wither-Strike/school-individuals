9c3918fedc1a6eee617fcfe5f38c8f9e5d018291
Wither-Strike <119773610+Wither-Strike@users.noreply.github.com>
2023-04-05 16:46:19 -0700

    minor edit removing a useless print

--- indiv_done/nmap script.py
+++ indiv_done/nmap script.py
@@ -5,7 +5,6 @@ with open("indiv_done/test.txt", "r") as file:
     for line in file:
         line = line.strip()
         ip_Addr.append(line)
-        print(ip_Addr)
 
 for ipaddr in ip_Addr:
     os.system(f"nmap -sCV {ipaddr} -oN indiv_done/{ipaddr}.txt --unprivileged")
